b675e0411a9a1f706c3a334971bf5988d4a37212
dineshvadlamani522 <dineshvadla1996@gmail.com>
2025-03-31 14:37:02 +0530

    added latest files

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 COPY target/springboot-h2-ci-cd-0.0.1-SNAPSHOT.jar app.jar
 
 # Expose port 8080
-EXPOSE 8080
+EXPOSE 8081
 
 # Run the application
 ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (name, email) VALUES ('John Doe', 'john.doe@example.com');
+INSERT INTO customer (name, email) VALUES ('Jane Smith', 'jane.smith@example.com');
+INSERT INTO customer (name, email) VALUES ('Alice Johnson', 'alice.johnson@example.com');
+INSERT INTO customer (name, email) VALUES ('Bob Brown', 'bob.brown@example.com');
